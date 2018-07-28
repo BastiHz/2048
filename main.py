0549a96005a6fa06ba8550f659af064ba03fd5f6
BastiHz <41594266+BastiHz@users.noreply.github.com>
2018-07-28 13:16:54 +0200

    Changed the colors.

--- main.py
+++ main.py
@@ -15,11 +15,27 @@ SCREEN_SIZE = TILE_SIZE * BOARD_SIZE + MARGIN_WIDTH * 2 + GAP_WIDTH * (BOARD_SIZ
 FONT_SIZE = 30
 FONT = pg.font.SysFont("Comic Sans MS", FONT_SIZE)
 CHANCE_OF_4 = 0.1
-BACKGROUND_COLOR = (100, 100, 100)
-FONT_COLOR = (230, 230, 230)
-TILE_COLORS = {}
-for i in range(1, 18):
-    TILE_COLORS[2**i] = [int(j * 255) for j in colorsys.hsv_to_rgb(20 * i, 0.75, 1)]
+BACKGROUND_COLOR = (150, 150, 150)
+FONT_COLOR = (255, 255, 255)
+TILE_COLORS = {
+    2: (188, 32, 223),
+    4: (128, 32, 223),
+    8: (70, 32, 223),
+    16: (32, 51, 223),
+    32: (32, 108, 223),
+    64: (32, 166, 223),
+    128: (32, 223, 223),
+    256: (32, 223, 166),
+    512: (32, 223, 108),
+    1024: (32, 223, 51),
+    2048: (70, 223, 32),
+    4096: (128, 223, 32),
+    8192: (185, 223, 32),
+    16384: (223, 204, 32),
+    32768: (223, 147, 32),
+    65536: (223, 89, 32),
+    131072: (223, 32, 32)
+}
 DIRECTIONS = {
     pg.K_LEFT: "left",
     pg.K_RIGHT: "right",
@@ -69,8 +85,7 @@ class Board:
                     position=(
                         MARGIN_WIDTH + (TILE_SIZE + GAP_WIDTH) * x,
                         MARGIN_WIDTH + (TILE_SIZE + GAP_WIDTH) * y
-                    ),
-                    value = 1024
+                    )
                 )
                 self.board[x][y] = new_tile
                 self.tiles.add(new_tile)
