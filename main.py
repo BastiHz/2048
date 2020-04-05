ead6c217fa4315be4f06bbca4e708103a246e67c
bastihz <41594266+BastiHz@users.noreply.github.com>
2020-04-05 17:50:44 +0200

    Finish simplifying the code

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-"""Copyright (C) 2019 Sebastian Henz
+"""Copyright (C) 2020 Sebastian Henz
 
 This program is free software: you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
@@ -71,160 +71,17 @@ DIRECTION_ROTATIONS = {  # How many times to rotate depending on move direction.
     "right": 2,
     "down": 3
 }
-
-
-
-
-
-# class Tile:
-#     def __init__(self, grid_x, grid_y, value=None):
-#         if value is not None:
-#             self.value = value
-#         else:
-#             self.value = 4 if random.random() < CHANCE_OF_2 else 2
-#         self.position = (0, 0)
-#         self.color = TILE_COLORS[self.value]
-#         self.surface = pg.Surface((TILE_SIZE, TILE_SIZE))
-#         self.rect = self.surface.get_rect()
-#         self.surface.fill(self.color)
-#         number_surf = FONT.render(str(self.value), True, FONT_COLOR, self.color)
-#         number_rect = number_surf.get_rect()
-#         number_rect.center = self.rect.center
-#         self.surface.blit(number_surf, number_rect)
-#         self.update_position(grid_x, grid_y)
-#         self.merge_lock = True  # If True tile will not merge with others.
-#
-#     def update_position(self, x, y):
-#         self.position = (
-#             GAP_WIDTH + (TILE_SIZE + GAP_WIDTH) * x,
-#             GAP_WIDTH + (TILE_SIZE + GAP_WIDTH) * y
-#         )
-#         self.rect.topleft = self.position
-#
-#     def draw(self):
-#         screen.blit(self.surface, self.rect)
-
-
-# class Board:
-#     def __init__(self):
-#         self.board = [[None for y in range(BOARD_SIZE)]
-#                       for x in range(BOARD_SIZE)]
-#         self.tiles = set()
-#
-#         self.surface = pg.Surface((SCREEN_SIZE, SCREEN_SIZE))
-#         self.surface.fill(BACKGROUND_COLOR)
-#         smaller_margin = GAP_WIDTH - BACKGROUND_PATTERN_OVERSIZE // 2
-#         for x in range(BOARD_SIZE):
-#             for y in range(BOARD_SIZE):
-#                 pg.draw.rect(
-#                     self.surface,
-#                     BACKGROUND_PATTERN_COLOR,
-#                     pg.Rect(
-#                         smaller_margin + (TILE_SIZE + GAP_WIDTH) * x,
-#                         smaller_margin + (TILE_SIZE + GAP_WIDTH) * y,
-#                         TILE_SIZE + BACKGROUND_PATTERN_OVERSIZE,
-#                         TILE_SIZE + BACKGROUND_PATTERN_OVERSIZE
-#                     )
-#                 )
-
-        # # Place the first two tiles:
-        # self.spawn_new_tile()
-        # self.spawn_new_tile()
-
-    # def spawn_new_tile(self, x=None, y=None, value=None):
-    #     if x is None and y is None:
-    #         while True:
-    #             x = random.choice(range(BOARD_SIZE))
-    #             y = random.choice(range(BOARD_SIZE))
-    #             if self.board[x][y] is None:
-    #                 break
-    #     new_tile = Tile(x, y, value)
-    #     self.board[x][y] = new_tile
-    #     self.tiles.add(new_tile)
-    #     # print(f"Spawned new tile at {x}, {y}, value = {value}")
-    #     # print("---")
-
-    # def handle_input(self, direction):
-    #     self.move_tiles(DIRECTION_ROTATION_LEFT[direction])
-
-    # def rotate_board(self, times):
-    #     for _ in range(times):
-    #         rotated_board = [i[:] for i in self.board]
-    #         for x in range(BOARD_SIZE):
-    #             for y in range(BOARD_SIZE):
-    #                 rotated_board[x][BOARD_SIZE - 1 - y] = self.board[y][x]
-    #         self.board = [i[:] for i in rotated_board]
-
-    # def move_tiles(self, rotate_n):
-    #     self.rotate_board(rotate_n)
-    #
-    #     spawn_new_random = False
-    #     something_changed = True
-    #     # Loop until the board does not change anymore.
-    #     while something_changed:
-    #         something_changed = False
-    #         for y in range(BOARD_SIZE):
-    #             for x in range(1, BOARD_SIZE):
-    #                 tile = self.board[x][y]
-    #                 if tile is None or (x - 1) < 0:
-    #                     continue
-    #
-    #                 left_tile = self.board[x - 1][y]
-    #                 if left_tile is None:
-    #                     self.board[x - 1][y] = tile
-    #                     self.board[x][y] = None
-    #                     something_changed = True
-    #                     spawn_new_random = True
-    #                 elif (left_tile.value == tile.value
-    #                       and not left_tile.merge_lock
-    #                       and not tile.merge_lock):
-    #                     new_value = tile.value * 2
-    #                     self.tiles.remove(left_tile)
-    #                     self.board[x - 1][y] = None
-    #                     self.tiles.remove(tile)
-    #                     self.board[x][y] = None
-    #                     self.spawn_new_tile(x - 1, y, new_value)
-    #                     something_changed = True
-    #                     spawn_new_random = True
-    #
-    #     if spawn_new_random:
-    #         self.spawn_new_tile()
-    #
-    #     self.rotate_board(4 - rotate_n)  # Rotate the board back.
-    #
-    #     for x in range(BOARD_SIZE):
-    #         for y in range(BOARD_SIZE):
-    #             tile = self.board[x][y]
-    #             if tile is not None:
-    #                 tile.update_position(x, y)
-    #                 tile.merge_lock = False
-
-    # def detect_loss(self):
-    #     # Scan the board and see if the player lost the game.
-    #     # If the board is full: for every tile: is there at least one
-    #     # neighbor with the same value so it can merge?
-    #     if len(self.tiles) < BOARD_SIZE * BOARD_SIZE:
-    #         return False
-    #     for x in range(BOARD_SIZE):
-    #         for y in range(BOARD_SIZE):
-    #             value = self.board[x][y].value
-    #             neighbors_xy = [
-    #                 [x - 1, y],
-    #                 [x + 1, y],
-    #                 [x, y - 1],
-    #                 [x, y + 1]
-    #             ]
-    #             for n in neighbors_xy:
-    #                 if (0 <= n[0] < 4) and (0 <= n[1] < 4):
-    #                     if self.board[n[0]][n[1]].value == value:
-    #                         return False
-    #     print("You lost the game.")
-    #     return True
-    #
-    # def draw(self):
-    #     screen.blit(self.surface, (0, 0))
-    #     for tile in self.tiles:
-    #         tile.draw()
+TILE_SURFACES = {}
+for p in range(1, 18):
+    value = 2**p
+    surface = pg.Surface((TILE_SIZE, TILE_SIZE))
+    color = TILE_COLORS[value]
+    surface.fill(color)
+    text_surface = FONT.render(str(value), True, FONT_COLOR, color)
+    text_rect = text_surface.get_rect()
+    text_rect.center = surface.get_rect().center
+    surface.blit(text_surface, text_rect)
+    TILE_SURFACES[value] = surface
 
 
 class Game:
@@ -307,12 +164,15 @@ class Game:
                     self.move_tiles(DIRECTIONS[e.key])
 
     def rotate_board(self, n):
-        """Rotate the board n times 90° clockwise."""
-        self.board = list(zip(*self.board))[::-1]
+        """Rotate the board n times 90° counterclockwise."""
+        for _ in range(n):
+            self.board = [list(column) for column in zip(*self.board[::-1])]
 
     def move_tiles(self, direction):
         n_rotations = DIRECTION_ROTATIONS[direction]
+        # print(self.board)
         self.rotate_board(n_rotations)
+        # print(self.board)
 
         locked_positions = set()  # cannot merge in this frame
         spawn_new = False
@@ -324,16 +184,18 @@ class Game:
                     value = self.board[x][y]
                     if value == 0:
                         continue
-                    for left_x in range(0, x):
-                        if (left_x, y) in locked_positions:
-                            continue
-                        if self.board[left_x][y] == 0:
-                            self.board[left_x][y] = value
-                            self.board[x][y] = 0
-                        elif self.board[left_x][y] == value:
-                            self.board[left_x][y] = value * 2
-                            locked_positions.add((left_x, y))
-                            self.board[x][y] = 0
+                    left_x = x -1
+                    if (left_x, y) in locked_positions:
+                        continue
+                    if self.board[left_x][y] == 0:
+                        self.board[left_x][y] = value
+                        self.board[x][y] = 0
+                        something_changed = True
+                        spawn_new = True
+                    elif self.board[left_x][y] == value:
+                        self.board[left_x][y] = value * 2
+                        locked_positions.add((left_x, y))
+                        self.board[x][y] = 0
                         something_changed = True
                         spawn_new = True
 
@@ -343,6 +205,14 @@ class Game:
 
     def draw(self):
         self.screen.blit(self.background, (0, 0))
+
+        for x, y in self.board_coordinates:
+            if self.board[x][y] > 0:
+                position = (
+                    GAP_WIDTH + (TILE_SIZE + GAP_WIDTH) * x,
+                    GAP_WIDTH + (TILE_SIZE + GAP_WIDTH) * y
+                )
+                self.screen.blit(TILE_SURFACES[self.board[x][y]], position)
 
         pg.display.update()
 
